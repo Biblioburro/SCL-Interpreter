8af7aa6611bad15349d40e4206dd3940436e2614
TsunamiMonsoon <55356057+TsunamiMonsoon@users.noreply.github.com>
2021-02-12 18:58:53 -0500

    Add files via upload
    Possible Scanner??

--- SCL_Interpreter/src/CS4308/ConceptsOfProgramming/Main.java
+++ SCL_Interpreter/src/CS4308/ConceptsOfProgramming/Main.java
@@ -3,6 +3,27 @@ package CS4308.ConceptsOfProgramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner("C:\\Users\\heath\\OneDrive\\Desktop\\testing.txt"); // here we scan the given file
+
+        String fileName = "testing.txt"; // name of the file
+
+        System.out.println("Start reading to scan file: " + fileName); // print what file it is reading
+        System.out.println();
+
+        // while scanning print the current lex and token
+        while(!scan.isDone()) {
+            // formatting: '-' is left aligned, 18 fills the string up to 18 chars, '%' means what follows is an argument that will be formatted
+            System.out.printf("%-18s :  %s \n", scan.currentLex(), scan.currentToken());
+            scan.moveAhead();
+        }
+
+        // if scan is successful print it was successful
+        if(scan.isSuccessful()) {
+            System.out.println("SCAN IS SUCCESSFUL!");
+        }
+        // otherwise print an error message
+        else {
+            System.out.println(scan.errorMess());
+        }
     }
 }
